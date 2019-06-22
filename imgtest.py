7f1261faae7f1f6ff2aa2899252c1219c4fe80f8
Ranxin <565640524@qq.com>
2019-06-22 10:56:58 +0800

    imencode test2

--- imgtest.py
+++ imgtest.py
@@ -1,9 +1,12 @@
 import cv2
 import numpy as np
 
-img1 = cv2.imread('drunk.jpg')
+img1 = cv2.imread('drunk2.jpg')
 bytes1 = img1.tobytes()
 print(type(img1), img1.shape)
+# cv2.imshow('img_decode', img1)
+# cv2.waitKey()
+
 
 # img2 = np.frombuffer(bytes1, dtype=np.uint8).reshape([4000, 3000, 3])[:, :, ::-1]
 # print(len(bytes1))
@@ -18,8 +21,8 @@ print(type(img1), img1.shape)
 img_str = cv2.imencode('.jpg', img1)[1].tobytes()
 print(type(img_str), len(img_str), len(bytes1))
 image = np.asarray(bytearray(img_str), dtype="uint8")
-img3 = cv2.imdecode(image, cv2.IMREAD_COLOR)
+img3 = cv2.imdecode(image, cv2.IMREAD_ANYCOLOR)
 print(img3 == img1)
 print(type(img3), img3.shape)
-# cv2.imshow('img_decode',image)
-# cv2.waitKey()
+cv2.imshow('img_decode', img3)
+cv2.waitKey()
