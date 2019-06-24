842dd8d168e136a6351f08a235ebce7965d63ae5
Ranxin <565640524@qq.com>
2019-06-24 15:01:54 +0800

    implemented drowsy normal streaming

--- drunk_streaming_structured.py
+++ drunk_streaming_structured.py
@@ -16,7 +16,7 @@ from pyspark.sql.functions import *
 input_topic = 'input'
 output_topic = 'output'
 brokers = "G01-01:2181,G01-02:2181,G01-03:2181,G01-04:2181,G01-05:2181,G01-06:2181,G01-07:2181,G01-08:2181," \
-          "G01-09:2181,G01-10:2181,G01-11:2181,G01-12:2181,G01-13:2181,G01-14:2181,G01-15:2181,G01-16:2181 "
+          "G01-09:2181,G01-10:2181,G01-11:2181,G01-12:2181,G01-13:2181,G01-14:2181,G01-15:2181,G01-16:2181"
 
 csv_file_path = "file:///home/hduser/DrunkDetection/train_data48.csv"
 predictor_path = "/home/hduser/DrunkDetection/shape_predictor_68_face_landmarks.dat"
