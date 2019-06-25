9275f2ca188fc11326e7a39604f0b948b877ffa0
stormrise <lilingxiao0414@gmail.com>
2019-06-25 21:18:56 +0800

    distracted init

---
+++ distracted_drivers_keras.py
@@ -0,0 +1,5 @@
+import keras
+
+# img size 224*224
+keras.applications.mobilenet_v2.MobileNetV2(input_shape=None, alpha=1.0, depth_multiplier=1, include_top=True,
+                                            weights='imagenet', input_tensor=None, pooling=None, classes=1000)
