5c7ecdb2effdf27534060f528231d8923b971e4d
Ranxin <565640524@qq.com>
2019-07-05 17:11:23 +0800

    partition 15

--- drowsy_streaming.py
+++ drowsy_streaming.py
@@ -35,7 +35,7 @@ def eye_aspect_ratio(eye):
     return ear
 
 
-kafkaStream = KafkaUtils.createStream(ssc, brokers, 'test-consumer-group', {input_topic: 10},
+kafkaStream = KafkaUtils.createStream(ssc, brokers, 'test-consumer-group', {input_topic: 15},
                                       valueDecoder=my_decoder)
 producer = KafkaProducer(bootstrap_servers='G01-01:9092', compression_type='gzip', batch_size=163840,
                          buffer_memory=33554432, max_request_size=20485760)
